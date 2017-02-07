1fb6b17850fb111689a2043da336b868485f927f
Anthony Carrick <anthonix@gmail.com>
2017-02-07 14:57:00 +1000

    Add project files.

---
+++ Character Formatting/Unicode Stripper.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Character_Formatting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input_box = new System.Windows.Forms.TextBox();
+            this.output_box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bad_characters_output = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.trim_flag = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // input_box
+            // 
+            this.input_box.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.input_box.Location = new System.Drawing.Point(150, 3);
+            this.input_box.Multiline = true;
+            this.input_box.Name = "input_box";
+            this.input_box.Size = new System.Drawing.Size(310, 24);
+            this.input_box.TabIndex = 0;
+            this.input_box.TextChanged += new System.EventHandler(this.input_box_TextChanged);
+            // 
+            // output_box
+            // 
+            this.output_box.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.output_box.Location = new System.Drawing.Point(150, 59);
+            this.output_box.Multiline = true;
+            this.output_box.Name = "output_box";
+            this.output_box.Size = new System.Drawing.Size(310, 24);
+            this.output_box.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Safe Characters";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(3, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Bad Characters";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // bad_characters_output
+            // 
+            this.bad_characters_output.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bad_characters_output.Location = new System.Drawing.Point(150, 33);
+            this.bad_characters_output.Name = "bad_characters_output";
+            this.bad_characters_output.ReadOnly = true;
+            this.bad_characters_output.Size = new System.Drawing.Size(310, 20);
+            this.bad_characters_output.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(150, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(310, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(3, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 26);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Positions of bad chars";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 147F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.input_box, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.bad_characters_output, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.output_box, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.trim_flag, 1, 4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 22F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(463, 134);
+            this.tableLayoutPanel1.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Test to Check";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Location = new System.Drawing.Point(3, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(141, 22);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Trim text prior to make-safe";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // trim_flag
+            // 
+            this.trim_flag.AutoSize = true;
+            this.trim_flag.Checked = true;
+            this.trim_flag.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.trim_flag.Location = new System.Drawing.Point(150, 115);
+            this.trim_flag.Name = "trim_flag";
+            this.trim_flag.Size = new System.Drawing.Size(66, 16);
+            this.trim_flag.TabIndex = 11;
+            this.trim_flag.Text = "Will Trim";
+            this.trim_flag.UseVisualStyleBackColor = true;
+            this.trim_flag.CheckedChanged += new System.EventHandler(this.trim_flag_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 163);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Text = "Unicode Stripper";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input_box;
+        private System.Windows.Forms.TextBox output_box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox bad_characters_output;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox trim_flag;
+    }
+}
+
---
+++ Character Formatting/Unicode Stripper.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Character_Formatting
+{
+    public partial class Form1 : Form
+    {
+        Encoding ascii = Encoding.ASCII;
+        Encoding unicode = Encoding.Unicode;
+        List<int> offendingChars = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private bool inList(List<int> list, int thing)
+        {
+            bool test = false;
+
+            foreach (int item in list)
+            {
+                if (thing == item)
+                {
+                    return true;
+                }
+            }
+            return test;
+        }
+
+        private void show_bad_chars()
+        {
+            textBox1.Text = "";
+            bad_characters_output.Text = "";
+            Console.Write(offendingChars.ToString());
+            textBox1.Text = string.Join(", ", offendingChars.ToArray());
+            
+            for (int i = 0; i < input_box.Text.Length; i++)
+            {
+               if (inList(offendingChars, i))
+                {
+                    bad_characters_output.Text = bad_characters_output.Text + input_box.Text[i];
+                }
+               else
+                {
+                    bad_characters_output.Text = bad_characters_output.Text + " ";
+                }
+            }
+        }
+
+        private void input_box_TextChanged(object sender, EventArgs e)
+        {
+            //reset the strings so we can get them fresh each time
+              string text_string = input_box.Text;
+            //trim if checked
+            if (trim_flag.Checked)
+            {
+                text_string = text_string.Trim();
+            }
+            offendingChars = new List<int>();
+            output_box.Text = "";
+            string output_string = "";
+
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(text_string);
+            char[] chars = Encoding.ASCII.GetChars(asciiBytes);
+            string line = new String(chars);
+
+            int i = 0;
+            
+
+            foreach (var ch in line)
+            {
+
+                if (ch == 63)
+                {
+                    if (text_string[i] == '?')
+                    {
+                        //byte[] oneChar = new byte[ch];
+                        output_string = output_string + ch;
+                    }
+                    else
+                    {
+                        //this char is unicode so store its index away for later
+                        offendingChars.Add(i);
+                    }
+                }
+                else
+                {
+                    //byte[] oneChar = new byte[ch];
+                    output_string = output_string + ch;
+                }
+
+                //last thing
+                i++;
+            }
+            if (trim_flag.Checked) {
+                output_string = output_string.Trim();
+            }
+            output_box.Text = output_string;
+
+            //Finally, show the bad characters
+            show_bad_chars();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trim_flag_CheckedChanged(object sender, EventArgs e)
+        {
+            if (trim_flag.Checked) {
+                trim_flag.Text = "Will Trim";
+            }
+            else
+            {
+                trim_flag.Text = "Won't Trim";
+            }
+        }
+    }
+}
